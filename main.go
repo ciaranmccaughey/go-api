a7dae26dd97a0329a063b37f87d37dad28c0c961
Ciaran <mccaugheyciaran@gmail.com>
2021-05-20 20:44:33 +1000

    Added router and routes.

--- main.go
+++ main.go
@@ -5,15 +5,24 @@ import (
 	"fmt",
 	"log",
 	"net/http",
-	"github.com/gorilla/mux"
+	"github.com/gorilla/mux",
+	"github.com/subosito/gotenv"
 )
 
 type Book struct {
-	ID string `json:id`
+	ID int `json:id`
 	Title string `json:id`
 	Author string `json:id`
 	Year string `json:id`
 }
 
+var books []Book;
+
 func main() {
+	router := mux.NewRouter()
+    router.HandleFunc("/books", getBooks).Methods.("GET");
+    router.HandleFunc("/books/{id}", getBook).Methods.("GET");
+    router.HandleFunc("/books", addBook).Methods.("POST");
+    router.HandleFunc("/books", updateBook).Methods.("PUT");
+    router.HandleFunc("/books/{id}", removeBook).Methods.("DELETE");
 }
